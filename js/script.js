7a520d27b88a087ce44053897a3e58a6ee64ef6d
Пил Пол <a@MacBook-Pro-Pil.local>
2020-04-29 23:33:07 +0300

    making sliders

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+    $('.slider-container').slick({
+        arrows:false,
+        touchMove: true,
+        centerMode: true,
+        variableWidth: true,
+        centerPadding: '5000px',
+        slidesToShow: 2,
+    });
+  });
+  
