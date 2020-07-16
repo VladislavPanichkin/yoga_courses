6ad297db1232fe52577d0418763e4b5e6454fab9
VladislavPanichkin <54220242+VladislavPanichkin@users.noreply.github.com>
2020-07-16 23:34:46 +0300

    slider fixes + media fixes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
     $('.slider-container').slick({
         arrows: false,
+        dots: true,
         touchMove: true,
         centerMode: true,
         variableWidth: true,
         centerPadding: '5000px',
         slidesToShow: 1
     });
+
+    if (window.matchMedia("(max-width: 450px)").matches) {
+        // showing .grid-container as a slider only on mobiles
+        $('.grid-container').slick({
+            arrows: false,
+            dots: true,
+            touchMove: true,
+            centerMode: false,
+            variableWidth: false,
+            slidesToShow: 1
+        });
+    }
 
     $('.grid-gallery').slick({
         mobileFirst: true,
